40f3b72672bea9d3519c6dc83d54a372db09847d
0x-wen <libai7236@gmail.com>
2024-11-29 13:55:48 +0800

    test: update bitcoin.md

--- wallet/document/address/bitcoin.md
+++ wallet/document/address/bitcoin.md
@@ -3,11 +3,11 @@
 
 ### p2pkh 推导过程
 
-![img.png](picture/img.png)
+![img.png](../picture/img.png)
 
 - 传入一个公钥解析，02，03开头的是压缩公钥，其他类型为非压缩公钥
 - ripemd160(sha256(b)), 先将公钥进行sha256，再进行ripemd160
-![img.png](picture/img1.png)
+![img.png](../picture/img1.png)
 - 计算checksum, 主网PubKeyHashAddrID: 0x00,测试网:TestNetAddrID: 0x6f, 进行两次sha256，取前4位
 ```go
 h := sha256.Sum256(input)
@@ -16,3 +16,14 @@ copy(cksum[:], h2[:4])
 ```
 - [version + ripemd160_hash + checksum] 的切片数据，然后使用base58编码
 
+## 解锁脚本 与 锁定脚本
+通过地址 即可得到 锁定脚本，用于给接受方
+解锁脚本 用于发送方，需要signatureScript和publicKey
+
+### 隔离见证
+通过提取signatureScript,将数据放在witness中，确保了交易的延展性
+之前的标准地址，signatureScript根据ecdsa的签名算法，会出现(x,y)和(x,-y)同时签名一个utxo，从而导致txid不确定
+
+
+## taproot
+[taproot总结](https://aandds.com/blog/bitcoin-taproot.html)
--- wallet/document/address/privkey.py
+++ wallet/document/address/privkey.py
@@ -3,7 +3,8 @@ from ecdsa.ellipticcurve import PointJacobi
 
 
 def derive_public_key(private_key: bytes, compressed: bool = False) -> bytes:
-    Q: PointJacobi = int.from_bytes(private_key, byteorder='big') * ecdsa.curves.SECP256k1.generator
+    Q: PointJacobi = int.from_bytes(
+        private_key, byteorder='big') * ecdsa.curves.SECP256k1.generator
     xstr: bytes = Q.x().to_bytes(32, byteorder='big')
     ystr: bytes = Q.y().to_bytes(32, byteorder='big')
     if compressed:
@@ -13,8 +14,52 @@ def derive_public_key(private_key: bytes, compressed: bool = False) -> bytes:
         return b'\04' + xstr + ystr
 
 
-prikey = bytearray.fromhex('1e99423a4ed27608a15a2616a2b0e9e52ced330ac530edcc32c8ffc6a526aedd')
+prikey = bytearray.fromhex(
+    '1e99423a4ed27608a15a2616a2b0e9e52ced330ac530edcc32c8ffc6a526aedd')
 uncompressed_pubkey = derive_public_key(prikey, False)
 print("uncompressed public key =", uncompressed_pubkey.hex())
 compressed_pubkey = derive_public_key(prikey, True)
 print("compressed public key =", compressed_pubkey.hex())
+
+
+def create_private_key():
+    # 使用 SECP256k1 曲线生成私钥
+    private_key = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)
+    # 从私钥生成公钥
+    public_key = private_key.get_verifying_key()
+
+    # 导出私钥和公钥（十六进制形式）
+    private_key_hex = private_key.to_string().hex()
+    public_key_hex = public_key.to_string().hex()
+
+    # 公钥非压缩格式 (04 + x + y)
+    public_key_uncompressed = "04" + public_key_hex
+    print(f"-----------------------------------------------------------")
+    print(f"Private Key (hex): {private_key_hex}")
+    print(f"Public Key (uncompressed hex): {public_key_uncompressed}")
+    print(f"-----------------------------------------------------------")
+    private_key_bytes = private_key.to_string()
+    uncompressed_pubkey = derive_public_key(private_key_bytes, False)
+    print("uncompressed public key =", uncompressed_pubkey.hex())
+    compressed_pubkey = derive_public_key(private_key_bytes, True)
+    print("compressed public key =", compressed_pubkey.hex())
+
+    return private_key_hex, public_key_uncompressed
+
+
+def public_key():
+    """
+    比特币中使用 Schnorr 签名时，编码的公钥只是表示为 32 字节十六进制字符串的 x 坐标：
+    """
+    public_key = {
+        "x": 94143704248521553317086831157498059579898345832673799690735511018320990355030,
+        "y": 44438543306112247703620323006762464482367802894269621488396118668492541437765,
+    }
+
+    hex_string = hex(public_key.get("x"))
+    print(f"Schnorr 签名使用公钥: {hex_string[2:]}")
+
+
+if __name__ == "__main__":
+    public_key()
+    create_private_key()
